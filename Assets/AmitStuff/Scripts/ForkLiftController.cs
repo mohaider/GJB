bf71d61b9bdc3a51fda833311b19eeae41f55c74
mohaider <mrhaider@gmail.com>
2015-05-30 19:11:14 -0400

    updated gitignore

---
+++ Assets/AmitStuff/Scripts/ForkLiftController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForkLiftController : MonoBehaviour {
+
+	Animator forkliftAnimation;
+	// Use this for initialization
+	void Start () {
+		forkliftAnimation = GameObject.Find ("ForkLift_Animated").GetComponent<Animator> ();
+	//	forkliftAnimation.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
