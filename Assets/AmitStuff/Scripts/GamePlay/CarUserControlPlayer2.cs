e07cebfa8791eaac37bf961343bd8c11c1d60d24
Amit Birkas <amitbirkas@gmail.com>
2015-05-31 08:34:51 -0400

    fa

--- Assets/AmitStuff/Scripts/GamePlay/CarUserControlPlayer2.cs
+++ Assets/AmitStuff/Scripts/GamePlay/CarUserControlPlayer2.cs
@@ -8,6 +8,7 @@ namespace UnityStandardAssets.Vehicles.Car
 	[RequireComponent(typeof (CarController))]
 	public class CarUserControl2Player2 : MonoBehaviour
 	{
+		private ConfigurableJoint cj ;
 		private CarController m_Car; // the car controller we want to use
 		public bool UsingKeyboard;
 		
@@ -15,23 +16,27 @@ namespace UnityStandardAssets.Vehicles.Car
 		{
 			// get the car controller
 			m_Car = GetComponent<CarController>();
+			cj = GetComponent<ConfigurableJoint> ();
+			GameObject oj = GameObject.FindGameObjectWithTag ("Fork");
+			cj.connectedBody = oj.GetComponent<Rigidbody> ();
 		}
 		
 		
 		private void FixedUpdate()
 		{
 			// pass the input to the car!
-			float h = CrossPlatformInputManager.GetAxis("Horizontal");
-			float v = CrossPlatformInputManager.GetAxis("Vertical");
+			float h = CrossPlatformInputManager.GetAxis("HorizontalPlayer2");
+			float v = CrossPlatformInputManager.GetAxis("VerticalPlayer2");
 			
 			float horizontalRightStick = CrossPlatformInputManager.GetAxis ("Player2RT");
-			float verticalRightStick = CrossPlatformInputManager.GetAxis ("Player2LT");
-			//Debug.Log ("hv " +horizontalRightStick);
-			//Debug.Log ("vv "+verticalRightStick);
+			float verticalRightStick = CrossPlatformInputManager.GetAxis ("Player2LT")*-1f;
+			float output = horizontalRightStick + verticalRightStick;
+			Debug.Log ("hv " +horizontalRightStick);
+			Debug.Log ("vv "+verticalRightStick);
 			#if !MOBILE_INPUT
 			float handbrake = CrossPlatformInputManager.GetAxis("Jump");
 			if(!UsingKeyboard)
-				m_Car.Move(h, verticalRightStick, verticalRightStick, handbrake);
+				m_Car.Move(h, output, output, handbrake);
 			else
 				m_Car.Move(h, v, v, handbrake);
 			#else
