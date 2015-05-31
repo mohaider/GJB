f566b447f206169aef953de3df7daaf2a27ed3f2
Amit Birkas <amitbirkas@gmail.com>
2015-05-31 01:14:19 -0400

    sync

---
+++ Assets/InGameUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameUI : MonoBehaviour {
+
+	bool inMenu = false;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("XboxB")) {
+			Application.LoadLevel(Application.loadedLevelName);
+		}
+		if (Input.GetButton("XboxStart")) {
+			inMenu = !inMenu;
+		}
+
+		if (inMenu) {
+			if (Input.GetButton ("Submit")) {
+				Application.LoadLevel(0);
+			}
+		}
+	}
+}
---
+++ Assets/MainMenuXBoxController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuXBoxController : MonoBehaviour {
+	public UnityEngine.UI.Button[] buttonsTopDown;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.B)) {
+			buttonsTopDown [0].SendMessage ("OnClick");
+		}
+	
+	}
+}
