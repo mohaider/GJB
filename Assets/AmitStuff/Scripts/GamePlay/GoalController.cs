7bdbceaf0d50cd9cb5fdce2fff98038fb849a0d8
Amit Birkas <amitbirkas@gmail.com>
2015-05-31 09:20:28 -0400

    GOAL SCORE UPGRADED

--- Assets/AmitStuff/Scripts/GamePlay/GoalController.cs
+++ Assets/AmitStuff/Scripts/GamePlay/GoalController.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class GoalController : MonoBehaviour {
 
 	public string tagOfScoreableObjects = "Moveable";
+	public string[] tags = {"5", "10", "15", "20", "25"};
 	float lengthX, lengthZ;
 	int score = 0;
 	GameObject[] moveables;
+	GameObject[] score5;
+	GameObject[] score10;
+	GameObject[] score15;
+	GameObject[] score20;
+	GameObject[] score25;
 	bool first = true;
 
 	public string nextMap;
@@ -17,6 +23,13 @@ public class GoalController : MonoBehaviour {
 
 	void Start() {
 		moveables = GameObject.FindGameObjectsWithTag(tagOfScoreableObjects);
+
+		score5 = GameObject.FindGameObjectsWithTag (tags [0]);
+		score10 = GameObject.FindGameObjectsWithTag (tags [1]);
+		score15 = GameObject.FindGameObjectsWithTag (tags [2]);
+		score20 = GameObject.FindGameObjectsWithTag (tags [3]);
+		score25 = GameObject.FindGameObjectsWithTag (tags [4]);
+
 		lengthX = this.gameObject.GetComponent<Renderer> ().bounds.extents.x;
 		lengthZ = this.gameObject.GetComponent<Renderer> ().bounds.extents.z;
 	}
@@ -25,18 +38,92 @@ public class GoalController : MonoBehaviour {
 		Debug.Log (col.name);
 		if ((col.name.Contains ("Fork") || col.name.Contains("CrashBlocker")) && first) {
 			Debug.Log("SCOREING");
-			foreach(GameObject m in moveables) {
-				if ((m.transform.position.x > this.transform.position.x - lengthX)
-				    &&
-				    (m.transform.position.x < this.transform.position.x + lengthX)
-				    &&
-				    (m.transform.position.z > this.transform.position.z - lengthZ)
-				    &&
-				    (m.transform.position.z < this.transform.position.z + lengthZ)
-				   ) {
-					score++;
+			if (moveables.Length != 0) {
+				foreach(GameObject m in moveables) {
+					if ((m.transform.position.x > this.transform.position.x - lengthX)
+					    &&
+					    (m.transform.position.x < this.transform.position.x + lengthX)
+					    &&
+					    (m.transform.position.z > this.transform.position.z - lengthZ)
+					    &&
+					    (m.transform.position.z < this.transform.position.z + lengthZ)
+					    ) {
+						score++;
+					}
 				}
 			}
+			if (score5.Length != 0) {
+				foreach(GameObject m in score5) {
+					if ((m.transform.position.x > this.transform.position.x - lengthX)
+					    &&
+					    (m.transform.position.x < this.transform.position.x + lengthX)
+					    &&
+					    (m.transform.position.z > this.transform.position.z - lengthZ)
+					    &&
+					    (m.transform.position.z < this.transform.position.z + lengthZ)
+					    ) {
+						score+=5;
+					}
+				}
+			}
+			if (score10.Length != 0) {
+				foreach(GameObject m in score10) {
+					if ((m.transform.position.x > this.transform.position.x - lengthX)
+					    &&
+					    (m.transform.position.x < this.transform.position.x + lengthX)
+					    &&
+					    (m.transform.position.z > this.transform.position.z - lengthZ)
+					    &&
+					    (m.transform.position.z < this.transform.position.z + lengthZ)
+					    ) {
+						score+=10;
+					}
+				}
+			}
+			if (score15.Length != 0) {
+				foreach(GameObject m in score15) {
+					if ((m.transform.position.x > this.transform.position.x - lengthX)
+					    &&
+					    (m.transform.position.x < this.transform.position.x + lengthX)
+					    &&
+					    (m.transform.position.z > this.transform.position.z - lengthZ)
+					    &&
+					    (m.transform.position.z < this.transform.position.z + lengthZ)
+					    ) {
+						score+=15;
+					}
+				}
+			}
+			if (score20.Length != 0) {
+				foreach(GameObject m in score20) {
+					if ((m.transform.position.x > this.transform.position.x - lengthX)
+					    &&
+					    (m.transform.position.x < this.transform.position.x + lengthX)
+					    &&
+					    (m.transform.position.z > this.transform.position.z - lengthZ)
+					    &&
+					    (m.transform.position.z < this.transform.position.z + lengthZ)
+					    ) {
+						score+=20;
+					}
+				}
+			}
+			if (score25.Length != 0) {
+				foreach(GameObject m in score25) {
+					if ((m.transform.position.x > this.transform.position.x - lengthX)
+					    &&
+					    (m.transform.position.x < this.transform.position.x + lengthX)
+					    &&
+					    (m.transform.position.z > this.transform.position.z - lengthZ)
+					    &&
+					    (m.transform.position.z < this.transform.position.z + lengthZ)
+					    ) {
+						score+=25;
+					}
+				}
+			}
+
+
 			nextmap = true;
 			first = false;
 		}
---
+++ Assets/Scripts/GameLogic/Director/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+	public enum NumberOfPlayers{
+		single, 
+		triple
+	}
+	private static InputManager _instance;
+//	public NumberOfPlayers  numOfPlayers;
+//	public InputManager Instance{
+//		get{
+//			if(_instance == null)
+//			{
+//				GameObject insta = GameObject.FindGameObjectWithTag("InputManager");
+//				if(insta !=null)
+//				{_instance = insta.GetComponent<InputManager>();
+//				if (_instance == null)
+//				{
+//					_instance = insta.AddComponent<InputManager>();
+//				}
+//			}
+//				else{
+//					insta = new GameObject();
+//					insta.tag = "InputManager";
+//					_instance = insta.AddComponent<InputManager>();
+//				}
+//			}
+//
+//		}
+//
+//
+//	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
