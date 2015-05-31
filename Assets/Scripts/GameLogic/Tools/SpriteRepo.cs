67a9e75966afc93eea56abaf866bacceca07dcae
mohaider <mrhaider@gmail.com>
2015-05-31 00:09:06 -0400

    explosion. massive explosions

---
+++ Assets/Scripts/GameLogic/Tools/SpriteRepo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Resources.Code.Tools
+{
+    public class SpriteRepo:MonoBehaviour
+    {
+        private static SpriteRepo m_instance;
+        public Sprite m_monkeySprite;
+        public Sprite m_giraffeSprite;
+      
+ 
+   
+        public static SpriteRepo Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    m_instance = SceneObjectFinder<SpriteRepo>.FindGameObjectReturnT("SpriteRepo");
+                    DontDestroyOnLoad(m_instance.gameObject);
+                }
+                return m_instance;
+            }
+            set { m_instance = value; }
+        }
+
+        public Sprite MMonkeySprite
+        {
+            get
+            {
+                if (m_monkeySprite == null)
+                {
+                    m_monkeySprite = UnityEngine.Resources.Load<Sprite>("Art/Sprites/Heads/MonkeyHead") ;
+                }
+                return m_monkeySprite;
+            }
+            set { m_monkeySprite = value; }
+        }
+
+        public Sprite MGiraffeSprite
+        {
+            get
+            {
+                if (m_giraffeSprite == null)
+                {
+                    m_giraffeSprite = UnityEngine.Resources.Load<Sprite>("Art/Sprites/Heads/GiraffeHead") ;
+                }
+                return m_giraffeSprite;
+            }
+            set { m_giraffeSprite = value; }
+        }
+    }
+}
---
+++ Assets/Scripts/GameLogic/items/ExplodableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodableObject : MonoBehaviour {
+
+	public GameObject Explosion;
+	private float timerToDissapear = 0.02f;
+	private bool isBlowingUp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
